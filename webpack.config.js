3bbff66e335cf0a8f355463ae4fb83bab5f8050d
wjb <wjb@wjbdeMacBook-Pro.local>
2021-01-05 23:44:10 +0800

    a

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+      index: './src/index.js', 
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: { 
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets:false
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+        
+    ],
+};
